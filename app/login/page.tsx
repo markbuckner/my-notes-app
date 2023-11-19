0cdae0ee69f54e286b23127234a19bc20091a24b
markbuckner <77910739+markbuckner@users.noreply.github.com>
2023-11-19 17:21:31 -0500

    Add login page

---
+++ app/login/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from 'react';
+import { supabase } from '../../supabaseClient';
+import { useRouter } from 'next/router';
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+    const router = useRouter();
+
+    const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const { error } = await supabase.auth.signInWithPassword({ email, password });
+        if (error) setMessage(error.message);
+        else router.push('/protected'); // Redirect to a protected page or home
+    };
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+            {message && <p>{message}</p>}
+        </div>
+    );
+}
