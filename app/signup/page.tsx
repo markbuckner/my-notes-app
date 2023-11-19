4830d1d3ede2738f05ec7918eefdf79e3f6e039b
markbuckner <77910739+markbuckner@users.noreply.github.com>
2023-11-19 16:48:11 -0500

    Try to fix signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from '../supabaseClient';
+import { supabase } from '../../supabaseClient';
 
 export default function SignUp() {
     const [email, setEmail] = useState('');
