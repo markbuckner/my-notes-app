7b42640fc461182abce01b65bc033c96ee389b89
markbuckner <77910739+markbuckner@users.noreply.github.com>
2023-11-20 19:56:02 -0500

    add box to home

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-// pages/index.tsx
-
 import Navbar from '../components/Navbar';
 
 const Home: React.FC = () => {
     return (
-        <div className="flex flex-col min-h-screen">
+        <>
             <Navbar />
-            <div className="flex-grow flex items-center justify-center">
-                <h1 className="text-4xl font-bold">Hello World!</h1>
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <div className="bg-white p-8 border rounded-lg shadow-lg">
+                    <h1 className="text-4xl font-bold text-center">Hello World!</h1>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
