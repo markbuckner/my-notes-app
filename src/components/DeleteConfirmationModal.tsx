16578e429df68c215259da2a656edb06cac66f75
markbuckner <77910739+markbuckner@users.noreply.github.com>
2023-12-10 23:38:33 -0500

    DeleteConfirmationModal added. Not currently used.

---
+++ src/components/DeleteConfirmationModal.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+type DeleteConfirmationModalProps = {
+  noteTitle: string;
+  onDeleteConfirm: () => void;
+  onCancel: () => void;
+};
+
+const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ noteTitle, onDeleteConfirm, onCancel }) => {
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center">
+      <div className="bg-white p-5 rounded-lg shadow-xl">
+        <h2 className="text-xl font-bold mb-4">Are you sure you want to delete this note?</h2>
+        <p>{noteTitle}</p>
+        <div className="flex justify-end mt-4">
+          <button onClick={onCancel} className="bg-gray-300 hover:bg-gray-400 text-black py-2 px-4 rounded mr-2">
+            No
+          </button>
+          <button onClick={onDeleteConfirm} className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded">
+            Yes
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteConfirmationModal;
