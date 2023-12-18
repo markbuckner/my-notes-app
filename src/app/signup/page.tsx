06078a023df16eb9c7d0acaf361b2a74d13ceab2
markbuckner <77910739+markbuckner@users.noreply.github.com>
2023-12-18 03:32:37 -0500

    captcha fixes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,52 +1,93 @@
 "use client";
-
-import { useState, FormEvent } from 'react';
+import React, { useState, FormEvent, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { createBrowserClient } from '@supabase/ssr';
 import Navbar from '../../components/Navbar';
 import withAuth from '../../components/WithAuth';
-import ReCAPTCHA from "react-google-recaptcha";
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const SignUp: React.FC<{ isLoggedIn: boolean }> = ({ isLoggedIn }) => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
   const [captchaToken, setCaptchaToken] = useState<string | null>(null);
   const [message, setMessage] = useState<string>('');
+  const [isCaptchaVerified, setIsCaptchaVerified] = useState(false);
   const router = useRouter();
   const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ''
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
   );
+
+  const onRecaptchaExpired = () => {
+    // Reset CAPTCHA state when it expires
+    setCaptchaToken(null);
+    setIsCaptchaVerified(false);
+  };
+
+  useEffect(() => {
+    // Clear any previous messages when the user starts typing again
+    setMessage('');
+    // Do not reset isCaptchaVerified here to persist its state across attempts
+  }, [email, password]);
 
   const handleSignUp = async (e: FormEvent) => {
     e.preventDefault();
 
-    // Call the server component for reCAPTCHA verification
-    if (captchaToken) {
-      const verificationResponse = await fetch('/api/verify-captcha', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ token: captchaToken }),
-      });
-
-      const verificationResult = await verificationResponse.json();
-      if (!verificationResult.success) {
-        setMessage("CAPTCHA verification failed. Please try again.");
-        setCaptchaToken(null); // Reset captcha token on failure
-        return;
-      }
-    } else {
-      setMessage("Please complete the CAPTCHA.");
+    // Check for missing email or password
+    if (!email || !password) {
+      setMessage('Please fill in all fields.');
       return;
     }
 
-    // If CAPTCHA is valid, proceed with sign up
-    const { error } = await supabase.auth.signUp({ email, password });
-    if (error) setMessage(error.message);
-    else {
-      router.push('/login');
+    if (password.length < 6) { // Assuming 6 is the minimum password length
+      setMessage('Password is too short.');
+      return;
+    }
+
+    if (!isCaptchaVerified) {
+      if (!captchaToken) {
+        setMessage('Please complete the CAPTCHA.');
+        return;
+      }
+
+      try {
+        const verificationResponse = await fetch('/api/verify-captcha', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ token: captchaToken }),
+        });
+
+        const verificationResult = await verificationResponse.json();
+        if (!verificationResult.success) {
+          setMessage('CAPTCHA verification failed. Please try again.');
+          setCaptchaToken(null); // Reset captcha token on failure
+          return;
+        }
+
+        setIsCaptchaVerified(true);
+      } catch (error: any) {
+        setMessage('An error occurred during CAPTCHA verification. Please try again.');
+        return;
+      }
+    }
+
+    // If CAPTCHA is verified, proceed with sign up
+    try {
+      const { error } = await supabase.auth.signUp({ email, password });
+      if (error) {
+        setMessage(error.message);
+        // Only reset CAPTCHA if the error is not 429 (Too Many Requests)?
+        if (error.status !== 429) { // 429: Too Many Requests
+          setIsCaptchaVerified(false);
+          setCaptchaToken(null);
+        }
+      } else {
+        router.push('/login');
+      }
+    } catch (error: any) {
+      setMessage('An error occurred during sign up. Please try again.');
     }
   };
 
@@ -78,7 +119,13 @@ const SignUp: React.FC<{ isLoggedIn: boolean }> = ({ isLoggedIn }) => {
             </div>
             <ReCAPTCHA
               sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY || ''}
-              onChange={setCaptchaToken}
+              onChange={(token) => {
+                setCaptchaToken(token);
+                if (token) {
+                  setIsCaptchaVerified(true);
+                }
+              }}
+              onExpired={onRecaptchaExpired} // Reset CAPTCHA state when expired
             />
             <div>
               <button
@@ -94,6 +141,6 @@ const SignUp: React.FC<{ isLoggedIn: boolean }> = ({ isLoggedIn }) => {
       </div>
     </>
   );
-}
+};
 
 export default withAuth(SignUp);
