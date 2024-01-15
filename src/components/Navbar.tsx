573308e5e69f64aa72b33b28277f809224b0f1bf
markbuckner <77910739+markbuckner@users.noreply.github.com>
2024-01-15 00:03:03 -0500

    max-w navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,11 +22,10 @@ const Navbar: React.FC<NavbarProps> = ({ onCreateNote, isNotesPage }) => {
   }
   return (
     <nav className="sticky top-0 z-20 bg-white shadow-md border-b-4 border-blue-500">
-      <div className="container mx-auto px-6 py-2 flex flex-col md:flex-row items-center">
-        <div className="flex flex-row items-center mb-1 md:mb-0">
-          <Link href="/" className="text-gray-800 font-semibold py-2 px-2 hover:text-purple-700 transition duration-300 ease-in-out">Home</Link>
+      <div className="max-w-full mx-auto px-6 py-2 flex flex-col md:flex-row items-center justify-between">
+        <div className="flex flex-row items-center mb-1 md:mb-0 space-x-3">
+          <Link href="/" className="text-gray-800 font-semibold py-2 px-4 hover:text-purple-700 transition duration-300 ease-in-out">Home</Link>
           <span className="text-gray-400 mx-3">|</span>
-
           {isLoggedIn ? (
             <>
               <Link href="/logout-confirm" className="text-gray-800 font-semibold py-2 px-4 hover:text-purple-700 transition duration-300 ease-in-out">Logout</Link>
@@ -43,15 +42,14 @@ const Navbar: React.FC<NavbarProps> = ({ onCreateNote, isNotesPage }) => {
           <span className="text-gray-400 mx-3">|</span>
           <Link href="/notes" className="text-gray-800 font-semibold py-2 px-2 hover:text-purple-700 transition duration-300 ease-in-out">Notes</Link>
         </div>
-
-        {/* Create New Note Button */}
         {isLoggedIn && isNotesPage && onCreateNote && (
-          <button
-            onClick={onCreateNote}
-            className="bg-blue-500 text-white rounded-md px-8 py-1 hover:bg-blue-600 md:ml-auto"
-          >
-            📝 New Note
-          </button>
+          <div className="flex flex-row-reverse md:flex-row md:ml-auto">
+            <button
+              onClick={onCreateNote}
+              className="bg-blue-500 text-white rounded-md px-8 py-1 hover:bg-blue-600 transition duration-300 ease-in-out">
+              📝 New Note
+            </button>
+          </div>
         )}
       </div>
     </nav>
