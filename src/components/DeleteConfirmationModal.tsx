331bc1b90416b4e2ed2567947f4bba3239d28646
markbuckner <77910739+markbuckner@users.noreply.github.com>
2024-02-02 20:48:07 -0500

    max-w-5xl, fix overflow

--- src/components/DeleteConfirmationModal.tsx
+++ src/components/DeleteConfirmationModal.tsx
@@ -11,7 +11,7 @@ const DeleteConfirmationModal: React.FC<DeleteConfirmationModalProps> = ({ noteT
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex justify-center items-center z-20">
       <div className="bg-white p-5 rounded-lg shadow-xl">
         <h2 className="text-xl font-bold mb-4">Are you sure you want to delete this note?</h2>
-        <p className="break-words max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-full">{noteTitle}</p>
+        <p className="break-words max-w-xs sm:max-w-md md:max-w-2xl lg:max-w-4xl xl:max-w-5xl">{noteTitle}</p>
 
         <div className="flex justify-end mt-4">
           <button onClick={onDeleteConfirm} className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded mr-2">
